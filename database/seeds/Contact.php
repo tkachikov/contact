d897993933fd445b81382f4cbbf6dab7cd1da509
A. Tkachikov <a.tkachikov@mail.ru>
2020-11-04 02:47:15 +0500

    Get max prepared for MySQL

--- database/seeds/Contact.php
+++ database/seeds/Contact.php
@@ -5,10 +5,18 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Output\ConsoleOutput;
+use Illuminate\Support\Carbon;
 
 
 class Contact extends Seeder
 {
+    public $max_contacts = null;
+    public $max_chunk    = null;
+
+    public function __construct()
+    {
+        $this->max_contacts = (int)1e6;
+    }
     /**
      * Run the database seeds.
      *
@@ -16,19 +24,20 @@ class Contact extends Seeder
      */
     public function run()
     {
-        $db = DB::table('contacts');
+        $date_start = Carbon::now();
 
-        $max_contacts = 1e6;
-        $max_chunk = 1e4;
+        $this->max_chunk = $this->getMaxChunk();
+
+        $db = DB::table('contacts');
 
         $output = new ConsoleOutput();
 
         $output->writeln('<info>Generate random data...</info>');
 
-        $progress = new ProgressBar($output, $max_contacts);
+        $progress = new ProgressBar($output, $this->max_contacts);
         $progress->start();
 
-        for ($i = 0; $i < $max_contacts; $i++) {
+        for ($i = 0; $i < $this->max_contacts; $i++) {
             $contacts[] = [
                 'first_name' => Str::random(rand(1, 50)),
                 'last_name' => Str::random(rand(1, 50)),
@@ -43,11 +52,13 @@ class Contact extends Seeder
 
         $output->writeln("\r\n<info>Insert into database...</info>");
 
-        $progress->setMaxSteps((int)($max_contacts / $max_chunk));
+        $progress->setMaxSteps((int)($this->max_contacts / $this->max_chunk) ?? 1);
         $progress->start();
 
-        foreach (collect($contacts)->chunk($max_chunk) as $contacts_chunk) {
+        foreach (collect($contacts)->chunk($this->max_chunk) as $contacts_chunk) {
             $db->insert($contacts_chunk->toArray());
+            $date_between = Carbon::now()->diffInSeconds($date_start);
+            $output->writeln("\r\n<info>Time: {$date_between}</info>");
             $progress->advance();
         }
 
@@ -57,12 +68,48 @@ class Contact extends Seeder
         return 0;
     }
 
+    public function getMaxChunk()
+    {
+        $driver = env('DB_CONNECTION') ?? Config::get('database.default');
+
+        if (! method_exists($this, ($method = 'getMaxChunkFor' . Str::ucfirst($driver)))) {
+            return (int)1e4;
+        }
+
+        return $this->{$method}();
+    }
+
+    public function getMaxChunkForMysql()
+    {
+        $pdo = [
+            PDO::ATTR_EMULATE_PREPARES => true,
+        ];
+
+        $options_key = 'database.connections.mysql.options';
+        $options = Config::get($options_key);
+
+        foreach ($pdo as $key => $val) {
+            if (! array_key_exists($key, $options)) {
+                Config::set("{$options_key}.{$key}", $val);
+            }
+        }
+
+        try {
+            DB::statement("set global max_prepared_stmt_count = {$this->max_contacts}");
+            return $this->max_contacts;
+        } catch (\Exception $e) {
+            return (int)DB::select("show variables like 'max_prepared_stmt_count'")[0]->Value;
+        }
+    }
+
     public function getPhone()
     {
         $max_length = rand(5, 50);
+
         for ($i = 0; $i < $max_length; $i++) {
             $phone[] = rand(0, 9);
         }
+
         return implode($phone);
     }
 }
